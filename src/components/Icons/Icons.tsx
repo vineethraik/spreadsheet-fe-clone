339147cc3c74278999058a0a7a6927b5b1c11f58
Vineeth Rai K <vineethraik23082000@gmail.com>
2025-07-08 12:29:45 +0530

    Update Icons component for style standards

--- src/components/Icons/Icons.tsx
+++ src/components/Icons/Icons.tsx
@@ -7,6 +7,10 @@ import globe from "../../assets/Globe.svg";
 import image_hi_fi from "../../assets/EmojiHiFi.svg";
 import downArrow from "../../assets/DownArrowFill.svg";
 import dropdown from "../../assets/Dropdown.svg";
+import refresh from "../../assets/Sync.svg";
+import split_gray from "../../assets/Split_gray.svg";
+import split from "../../assets/Split.svg";
+import more from "../../assets/More.svg";
 import type { HTMLAttributes } from "react";
 
 const IconUrls: { [key: string]: string } = {
@@ -17,25 +21,34 @@ const IconUrls: { [key: string]: string } = {
   in_fill_down_arrow: downArrow,
   avatar: avatar,
   globe: globe,
-  downward_forked_arrow_gray: "downward_forked_arrow_gray",
+  downward_forked_arrow_gray: split_gray,
+  downward_forked_arrow_white: split,
   emoji_hi_five: image_hi_fi,
   dropdown: dropdown,
+  refresh: refresh,
+  more_options: more,
 };
 
 export const Icons = ({
   iconId,
   style,
   className,
-  onClick
+  onClick,
 }: {
   iconId: string;
   style?: React.CSSProperties;
   className?: string;
-} &HTMLAttributes<HTMLImageElement>) => {
+} & HTMLAttributes<HTMLImageElement>) => {
   const iconSrc = `${IconUrls?.[iconId]}`;
   return (
     iconSrc && (
-      <img onClick={onClick} className={className} style={style} src={iconSrc} alt="" />
+      <img
+        onClick={onClick}
+        className={className}
+        style={{ width: "16px", height: "16px", ...style }}
+        src={iconSrc}
+        alt=""
+      />
     )
   );
 };
